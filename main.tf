98643b66cf5db301a4bfd67c51947b5582a3f641
az5890 <az5890@hotmail.com>
2023-06-25 20:22:18 +0530

    AAAA

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 }
 
 module "variable" {
-  source                   = "../../modules/"
+  source                   = "../../modules"
   resource_group_name      = "example-resource-group"
   virtual_network_name     = "example-virtual-network"
 }
