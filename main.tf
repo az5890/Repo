a38d4d305ffee591fb88ada9390f75eec0db4a39
az5890 <az5890@hotmail.com>
2023-06-25 19:55:34 +0530

    AS

--- main.tf
+++ main.tf
@@ -14,23 +14,10 @@ provider "azurerm" {
   }
 }
 
-variable "resource_group_name" {
-  description = "RG"
+module "example" {
+  source                   = "./example-module"
+  resource_group_name      = "example-resource-group"
+  virtual_network_name     = "example-virtual-network"
 }
 
-variable "virtual_network_name" {
-  description = "VNET"
-}
-
-resource "azurerm_resource_group" "example_resource_group" {
-  name     = var.resource_group_name
-  location = "East US"
-}
-
-resource "azurerm_virtual_network" "example_virtual_network" {
-  name                = var.virtual_network_name
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example_resource_group.location
-  resource_group_name = azurerm_resource_group.example_resource_group.name
-}
 
--- variable.tf
+++ variable.tf
@@ -5,3 +5,15 @@ variable "resource_group_name" {
 variable "virtual_network_name" {
   description = "VNET"
 }
+
+resource "azurerm_resource_group" "example_resource_group" {
+  name     = var.resource_group_name
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "example_virtual_network" {
+  name                = var.virtual_network_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example_resource_group.location
+  resource_group_name = azurerm_resource_group.example_resource_group.name
+}
