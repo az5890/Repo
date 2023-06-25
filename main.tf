7d0f72456f71f7c715fcac4616decbe828e90821
az5890 <az5890@hotmail.com>
2023-06-25 20:05:17 +0530

    A

--- main.tf
+++ main.tf
@@ -15,9 +15,10 @@ provider "azurerm" {
 }
 
 module "variable" {
-  source                   = "./example-module"
+  source                   = "./https://github.com/az5890/Repo/blob/main/variable.tf"
   resource_group_name      = "example-resource-group"
   virtual_network_name     = "example-virtual-network"
 }
 
 
+C:\Repo\variable.tf
