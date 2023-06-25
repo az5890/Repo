5d96d55e442c4c41358a62ba179448015972e941
az5890 <az5890@hotmail.com>
2023-06-25 20:24:20 +0530

    A

--- main.tf
+++ main.tf
@@ -13,9 +13,14 @@ provider "azurerm" {
   
   }
 }
-
-module "variable" {
-  source                   = "../../C:\Repo\variable.tf"
-  resource_group_name      = "example-resource-group"
-  virtual_network_name     = "example-virtual-network"
+# Define the resource group
+resource "azurerm_resource_group" "example" {
+  name     = "RG"
+  location = "East US"
 }
+
+# module "variable" {
+#   source                   = "../../C:\Repo\variable.tf"
+#   resource_group_name      = "example-resource-group"
+#   virtual_network_name     = "example-virtual-network"
+# }
