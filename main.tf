5e04ead45b0a0ba441581045d3e499f9be9bab67
az5890 <az5890@hotmail.com>
2023-06-26 07:50:10 +0530

    tc

--- main.tf
+++ main.tf
@@ -14,14 +14,8 @@ provider "azurerm" {
   }
 }
 
-# module "resource_group" {
-#   source = "./module-1"
-#   resource_group_name = rg.azurerm_resource_group
-#   location = rg.location
-# }
-
-# Define the resource group
-resource "azurerm_resource_group" "example" {
-  name     = "RG"
-  location = "East US"
+module "resource_group" {
+  source = "./module-rg"
+  rgdata = rg
 }
+
