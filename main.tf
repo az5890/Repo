70939b666269a40c303fd11a738a75d3b995029d
az5890 <az5890@hotmail.com>
2023-06-26 07:38:10 +0530

    sc

--- main.tf
+++ main.tf
@@ -14,8 +14,14 @@ provider "azurerm" {
   }
 }
 
-module "resource_group" {
-  source = "./module-1"
-  resource_group_name = rg.azurerm_resource_group
-  location = rg.location
+# module "resource_group" {
+#   source = "./module-1"
+#   resource_group_name = rg.azurerm_resource_group
+#   location = rg.location
+# }
+
+# Define the resource group
+resource "azurerm_resource_group" "example" {
+  name     = "RG"
+  location = "East US"
 }
